c50719f81b7414d0cad6286a371cc8fa38f867b4
Xiaodong <403533049@qq.com>
2020-05-23 16:38:53 +0800

    json read

--- 026.py
+++ 026.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
+
+import json
+with open('example.json', 'r') as myfile:
+	data = myfile.read()
+
+obj = json.loads(data)
+
+print("usd: " + str(obj['usd']))
+print("eur: " + str(obj['eur']))
+print("gbp: " + str(obj['gbp']))
